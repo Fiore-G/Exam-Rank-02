dfb6a209a3523d0154fdb19c9071ec112288b7a5
Fiorella G <fleurelle.g@gmail.com>
2025-07-18 15:07:06 +0200

    Update fizz_buzz.c

--- level_1/fizz_buzz.c
+++ level_1/fizz_buzz.c
@@ -14,11 +14,11 @@ int	main(void)
 	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			write(1, "fizzbuzz\n", 8);
-		else if (i % 3 == 0)
-			write(1, "fizz\n", 4);
+			write(1, "fizzbuzz\n", 9);
+	 else if (i % 3 == 0)
+			write(1, "fizz\n", 5);
 		else if (i % 5 == 0)
-			write(1, "buzz\n", 4);
+			write(1, "buzz\n", 5);
 		else
 			ft_putnbr(i);
 		write(1, "\n", 1);
