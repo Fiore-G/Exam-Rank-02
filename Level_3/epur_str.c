7b05515941f49e009c36ae20b93f745ae7d7ea09
Fiorella G <fleurelle.g@gmail.com>
2025-07-23 20:11:13 +0200

    Update epur_str.c

--- Level_3/epur_str.c
+++ Level_3/epur_str.c
@@ -14,22 +14,22 @@ int	main(int argc, char **argv)
 		int	i = 0;
 		char	*str = argv[1];
 		int	space = 0;
-
-		while (ft_isspace(str[i]))
-			i++;
+		
 		while (str[i])
 		{
-			if (ft_isspace(str[i]))
-				space = 1;
-			if (!ft_isspace(str[i]))
+			while (ft_isspace(str[i]))
+				i++;
+			if (str[i])
 			{
 				if (space)
 					write(1, " ", 1);
-				space = 0;
-				write(1, &str[i], 1);
+				while (str[i] && !ft_isspace(str[i]))
+				{
+					write(1, &str[i], 1);
+					i++;
+				}
+				space = 1;
 			}
-			i++;
-
 		}
 	}
 	write(1, "\n", 1);
