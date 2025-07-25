65cbcbb4444ae65eb595e5227188404d39c6f0fd
Fiorella G <fleurelle.g@gmail.com>
2025-07-25 14:54:21 +0200

    Update rstr_capitalizer.c

--- Level_3/rstr_capitalizer.c
+++ Level_3/rstr_capitalizer.c
@@ -1,16 +1,5 @@
 #include <unistd.h>
 
-void ft_putstr(char *s)
-{
-	int len;
-
-	len = 0;
-	while (s[len])
-		len++;
-	write(1, s, len);
-	write(1, "\n", 1);
-}
-
 int	ft_isspace(char c)
 {
 	if (c == ' ' || (c >= 9 && c <= 13))
@@ -18,18 +7,17 @@ int	ft_isspace(char c)
 	return (0);
 }
 
-void	r_capitalizer(char *str)
+void	rstr_capitalizer(char *str)
 {
 	int	i = 0;
 	
-	while (1)
+	while (str[i])
 	{
 		if (str[i] >= 'A' && str[i] <= 'Z')
 			str[i] += 32;
-		if (str[i] >= 'a' && str[i] <= 'z' && ft_isspace(str[i + 1]))
+		if (str[i] >= 'a' && str[i] <= 'z' && ft_isspace(str[i + 1]) || str[i + 1] == '\0')
 			str[i] -= 32;
-		if (str[i] >= 'a' && str[i] <= 'z' && str[i + 1] == '\0')
-			str[i] -= 32;
+		write(1, &str[i], 1);
 		i++;
 	}
 }
@@ -44,8 +32,8 @@ int	main(int argc, char **argv)
 	int	i = 1;
 	while (i < argc)
 	{
-		r_capitalizer(argv[i]);
-		ft_putstr(argv[i]);
+		rstr_capitalizer(argv[i]);
+		write(1, "\n", 1);
 		i++;
 	}
 	return (0);
