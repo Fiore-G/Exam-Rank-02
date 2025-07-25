d1e639513a34b8e775f35d1bbcd927da980fac7a
Fiorella G <fleurelle.g@gmail.com>
2025-07-25 12:27:56 +0200

    Update ft_atoi_base.c

--- Level_3/ft_atoi_base.c
+++ Level_3/ft_atoi_base.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
+int	is_valid(char c, int str_base)
+{
+	char	*lower_base = "0123456789abcdef";
+	char	*upper_base = "0123456789ABCDEF";
+	in	i = 0;
+	while (i < str_base)
+	{
+		if (c == lower_base[i] || c == upper_base[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 int	ft_atoi_base(const char *str, int str_base)
 {
 	int	i = 0;
 	int	result = 0;
 	int	sign = 1;
 
+	if (!str || str_base < 2 || str_base > 16)
+		return (0);
 	while (str[i] == ' '|| (str[i] >= 9 && str[i] <= 13))
 		i++;
 	if (str[i] == '-' || str[i] == '+')
@@ -14,14 +30,14 @@ int	ft_atoi_base(const char *str, int str_base)
 			sign = -1;
 		i++;
 	}
-	while (str[i])
+	while (str[i] && is_valid(str[i], str_base))
 	{
 		result *= str_base;
 		if (str[i] >= '0' && str[i] <= '9')
 			result += str[i] - '0';
-		else if (str[i] >= 'a' && str[i] <= 'z')
+		else if (str[i] >= 'a' && str[i] <= 'f')
 			result += str[i] - 'a' + 10;
-		else if (str[i] >= 'A' && str[i] <= 'Z')
+		else if (str[i] >= 'A' && str[i] <= 'F')
 			result += str[i] - 'A' + 10;
 		i++;
 	}
@@ -31,6 +47,6 @@ int	ft_atoi_base(const char *str, int str_base)
 int	main(void)
 {
 	char 	*s = "Fio";
-	printf("%d", ft_atoi_base(s, 10));
+	printf("%d", ft_atoi_base(s, 16));
 	return (0);
 }
